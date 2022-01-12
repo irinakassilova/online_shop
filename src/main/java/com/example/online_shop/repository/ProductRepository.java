999dc69be05bae166b6c3c4bcb7f73cc4695904c
Ирина Касилова <irinakasilova1@gmail.com>
2022-01-12 14:08:30 +0600

    Добавлены в репозиторий методы поиска товара по имени и описанию

--- src/main/java/com/example/online_shop/repository/ProductRepository.java
+++ src/main/java/com/example/online_shop/repository/ProductRepository.java
@@ -1,10 +1,16 @@
 package com.example.online_shop.repository;
 
 import com.example.online_shop.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     List<Product> findAllByName(String name);
+//    Page<Product> findAllByCategoryId(int id, Pageable pageable);
+    List<Product> findByNameStartingWithIgnoreCase(String name);
+    List<Product> findByDescriptionContainingIgnoreCase(String description);
+    List<Product> findByNameContainingIgnoreCase(String name);
 }
