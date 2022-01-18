e28f00c044dcad5b18b87dd8d602e54e3f053045
Ирина Касилова <irinakasilova1@gmail.com>
2022-01-18 12:56:37 +0600

    Добавлен CustomerController

--- src/main/java/com/example/online_shop/controller/CustomerController.java
+++ src/main/java/com/example/online_shop/controller/CustomerController.java
@@ -1,9 +1,14 @@
 package com.example.online_shop.controller;
 
+import com.example.online_shop.dto.CustomerDTO;
+import com.example.online_shop.service.CustomerService;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,4 +16,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @RequestMapping
 public class CustomerController {
+    private final CustomerService customerService;
+
+    @GetMapping(path = "/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+
+    }
+    @PostMapping("/register")
+    public String addRegister(CustomerDTO customerDTO, Model model) {
+        customerService.createCustomer(customerDTO);
+        return "redirect:/login";
+    }
 }
+
